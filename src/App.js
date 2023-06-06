3a67da08f56e5ff678100e0a3d47224e80c36df7
amitsharma2748 <amitji670@gmail.com>
2023-06-06 14:03:48 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import Products from "./components/Products.jsx";
-import ProductDetails from "./components/ProductDetails.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Routes, Route, HashRouter } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes className="App">
         <Route path="/" element={<Products />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
